c162393298078b5eb9b42690a49cf7839720fa83
Iaremenco <viniciusiaremenco@gmail.com>
2019-07-06 14:49:38 -0300

    Incluido identacao no retorno da api

--- AppCep/Startup.cs
+++ AppCep/Startup.cs
@@ -36,9 +36,10 @@ namespace AppCep
             string connection = @"Server=(localdb)\mssqllocaldb;Database=AppCep;Trusted_Connection=True;ConnectRetryCount=0";
 
             services.AddDbContext<Context>(options => options.UseSqlServer(connection));
-            
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddMvc().AddJsonOptions(options => options.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
